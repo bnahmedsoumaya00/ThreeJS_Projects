1ebd985521ff4c47b6779ccc4e45be860b8d179d
Soumayabenahmed <soumaya.benahmed@kelibia.r-iset.tn>
2025-08-28 22:59:10 +0100

    wireframe worm hole done

--- wireframe warmhole flying/index.js
+++ wireframe warmhole flying/index.js
@@ -1,42 +1,111 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import spline from './spline.js';
+import {EffectComposer} from "jsm/postprocessing/EffectComposer.js";
+import {RenderPass} from "jsm/postprocessing/RenderPass.js";
+import {UnrealBloomPass} from "jsm/postprocessing/UnrealBloomPass.js";
+// --- Setup the scene, camera & renderer ---
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-
-
 const scene = new THREE.Scene();
-
+scene.fog = new THREE.FogExp2(0x000000, 0.3);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
-
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 document.body.appendChild(renderer.domElement);
 
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
-scene.add(hemiLight);
-
+// --- End of Setup the scene, camera & renderer ---
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.3;
+
+//post-processing
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 0.85);
+bloomPass.threshold = 0;
+bloomPass.strength = 1.5;
+bloomPass.radius = 0.4;
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const line = new THREE.Line(geometry, material);
+//scene.add(line);
 
 
-function animate() {
+
+//create a tube geometry from the spline
+const tubeGeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMat = new THREE.MeshBasicMaterial({ 
+  color: 0xffffff,
+  wireframe: true,
+  side: THREE.DoubleSide,
+});
+const tube = new THREE.Mesh(tubeGeo, tubeMat);
+//scene.add(tube);
+
+//create edges geometry from the spline
+const edges = new THREE.EdgesGeometry(tubeGeo, 0.2);
+const lineMat = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const tubeLines = new THREE.LineSegments(edges, lineMat);
+scene.add(tubeLines);
+
+const numBoxes = 55;
+const size = 0.145;
+const boxGeo = new THREE.BoxGeometry(size, size, size);
+for(let i=0; i<numBoxes; i++){
+  const boxMat = new THREE.MeshBasicMaterial({ 
+    color: 0xffffff,
+    wireframe: true,
+  });
+  const box = new THREE.Mesh(boxGeo, boxMat);
+  const p = (i / numBoxes + Math.random() * 0.1) % 1;
+  const pos = tubeGeo.parameters.path.getPointAt(p);
+  pos.x += Math.random() - 0.4;
+  pos.z += Math.random() - 0.4;
+  box.position.copy(pos);
+  const rote = new THREE.Vector3(
+    Math.random() * Math.PI,
+    Math.random() * Math.PI,
+    Math.random() * Math.PI
+  );
+  box.rotation.copy(rote.x, rote.y, rote.z);
+  const edges = new THREE.EdgesGeometry(boxGeo, 0.2);
+  const color = new THREE.Color(Math.random(), Math.random(), Math.random());
+  const lineMat = new THREE.LineBasicMaterial({ color: color });
+  const boxLines = new THREE.LineSegments(edges, lineMat);
+  boxLines.position.copy(pos);
+  boxLines.rotation.copy(rote.x, rote.y, rote.z);
+  scene.add(boxLines);
+  // scene.add(box);
+}
+
+function updateCamera(t){
+  const time = t * 0.1;
+  const looptime = 5 * 1000;
+  const p = (time % looptime) / looptime;
+  const pos = tubeGeo.parameters.path.getPointAt(p);
+  const lookAt = tubeGeo.parameters.path.getPointAt((p + 0.03) % 1);
+  camera.position.copy(pos);
+  camera.lookAt(lookAt);
+}
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+
+function animate(t=0) {
   requestAnimationFrame(animate);
-
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.02;
-
-  renderer.render(scene, camera);
+  updateCamera(t);
+  composer.render(scene, camera);
+  controls.update();
 }
 animate();
 
